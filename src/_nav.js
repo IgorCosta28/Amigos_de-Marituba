49373a12329daa647d78882be82a16e4ea5915c2
IgorCosta28 <76978556+IgorCosta28@users.noreply.github.com>
2025-06-05 19:57:29 -0300

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -25,13 +25,13 @@ import {
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 
 const _nav = [
-  // {
-  //   component: CNavItem,
-  //   name: 'Dashboard',
-  //   type:'private',
-  //   to: '/dashboard',
-  //   icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
-  // },
+  {
+    component: CNavItem,
+    name: 'Dashboard',
+    type:'private',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  },
   {
     component: CNavTitle,
     type:'public',
